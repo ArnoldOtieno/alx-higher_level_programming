099369c5967be10edf029af3bd378a450c50f083
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-01-24 13:24:24 -0800

    data structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        z = len(my_list)
-        total = 0
-        for item in my_list:
-            if item not in my_list[:-z]:
-                total += item
-            z -= 1
-        return total
+    nlist = set(my_list)
+    return sum(nlist)
