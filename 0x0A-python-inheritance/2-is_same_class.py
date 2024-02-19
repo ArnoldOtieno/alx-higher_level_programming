6c1b553fae7e223c25aab06276c6093b5cd08e5f
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-02-19 12:05:34 -0800

    checking instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """obj is the instance being checked"""
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     else:
         return False
