dbd24fa41477f7da3ba24beb60db3dad0faddb4a
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-02-19 07:56:18 -0800

    lookup method

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 """this function returns list of available methods and attributes"""
 
 def lookup(obj):
     """store is the variable that stores the list"""
 
     store = list(dir(obj))
-    return store
+    return (store)
