f8dffd734e911482d603bb810bd9255cc8ead4f2
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-02-19 13:55:01 -0800

    immprove geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""geometry class"""
+
+
+class BaseGeometry:
+    """my base geometry class"""
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
