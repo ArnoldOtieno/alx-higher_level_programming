88332a57b879e52c7c08463838d6acd24286b0ef
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-02-20 12:15:45 -0800

    json encode

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns json representation"""
+import json
+
+def to_json_string(my_obj):
+    """The function that performs conversion"""
+    return json.dumps(my_obj)
