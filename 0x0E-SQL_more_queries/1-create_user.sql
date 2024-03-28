1a4e3360c2925840400a115c1287c168d80dcfa2
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-03-28 10:36:04 -0700

    Creating mysql user and setting password

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creating a user and setting password
-IF NOT EXISTS CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+IF NOT EXISTS CREATE USER user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
