e9643a3bdf68229e56e61905b452872cb79bbaf6
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-04-23 01:52:14 -0700

    correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const argslength = process.argv.length;
 if (argslength === 2) {
   console.log('No argument');
 } else if (argslength === 3) {
-  console.log('Argument Found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
