fc79d1cb9828c2b5ee680c6514d56325766b8c34
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-04-22 22:08:23 -0700

    Array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const marray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const count = marray.length;
+
+for (let i = 0; i < count; i++) {
+  console.log(marray[i]);
+}
