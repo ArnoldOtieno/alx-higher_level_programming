26f41f8d2978a2fcd9ae51911f97dc7efefdfbe4
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-04-22 19:16:20 -0700

    correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const arglength = process.argv.length;
+const argslength = process.argv.length;
 
-if (arglength === 2) {
+if (argslength === 2) {
   console.log('No argument');
-} else if (arglength === 3) {
+} else if (argslength === 3) {
   console.log('Argument Found');
 } else {
   console.log('Arguments found');
