98c461979e79324e826afe4b1ec389abb5659932
ArnolOtieno <arnoldotieno31@gmail.com>
2024-04-27 05:09:19 -0700

    filtering states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    pword = sys.argv[4]
 
     """Connecting to the database"""
     db = MySQLdb.connect(host='localhost', port=3306,
@@ -15,8 +14,8 @@ if __name__ == "__main__":
     """connecting cursor"""
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(pword))
+    cur.execute("SELECT * FROM states WHERE name = '{}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
