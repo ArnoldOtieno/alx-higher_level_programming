174fdfc4ae75aed0944d1cc5352edd940b7da70b
ArnolOtieno <arnoldotieno31@gmail.com>
2024-04-27 02:04:21 -0700

    filtering names starting in capital N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
+"""listing names starting in 'N'"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """connecting the database"""
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(user=username, passwd=password, db=database,
-                         host='localhost', port=3306)
+    """Connecting the database"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, passwd=password, db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    cur.execute("SELECT * FROM states ORDER BY id")
     [print(state) for state in cur.fetchall() if state[1][0] == "N"]
 
 
