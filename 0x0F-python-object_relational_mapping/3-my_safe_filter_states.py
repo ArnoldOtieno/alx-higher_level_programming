a6f7a7ef92551666204ca2bc20e044e687d21850
ArnolOtieno <arnoldotieno31@gmail.com>
2024-04-27 06:36:58 -0700

    safety from SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""search for a specific name"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    pword = sys.argv[4]
+
+    """Connecting to the database"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         passwd=password, db=database, user=username)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (pword,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
