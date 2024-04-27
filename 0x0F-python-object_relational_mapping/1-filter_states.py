87b1b10094df5961f96c2d110cb79faa2918a769
ArnolOtieno <arnoldotieno31@gmail.com>
2024-04-27 01:45:29 -0700

    filtering names starting in capital N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,10 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=username, passwd=password, db=database,
                          host='localhost', port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
+
 
     db.close()
