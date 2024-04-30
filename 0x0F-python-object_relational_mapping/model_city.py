cf5a68552e0e93673219147869958d29e4d26301
ArnolOtieno <arnoldotieno31@gmail.com>
2024-04-30 05:20:44 -0700

    model city creation

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""creating a class and objects"""
+from sqlalchemy import Column, String, Integer, MetaData
+from model_state import Base
+
+mymetadata = MetaData()
+
+class City(Base):
+    """city base class"""
+    __tablename__ = "cities"
+
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("state_id"), nullable=False)
+
