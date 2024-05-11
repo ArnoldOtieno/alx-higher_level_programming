59c8715de72ecd095cfed42414be01ebeb5ac379
ArnoldOtieno <arnoldotieno31@gmail.com>
2024-05-10 19:34:04 -0700

    fetching url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     q = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t - type: {}".format(type(q.text)))
-    print("\t - content: {}".format(q.text))
+    print("\t- type: {}".format(type(q.text)))
+    print("\t- content: {}".format(q.text))
